457475c1885f45944cc12ae6b45beed2554bc63f
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-10-10 19:18:54 +0300

    Created TrafficLight model and service TrafficLightService

---
+++ ExampleMVP/Services/TrafficLightService.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+final class TrafficLightService {
+    func getTrafficLight(colorName: (String), callBack: (TrafficLight?) -> Void) {
+        let trafficLight = [
+            TrafficLight(colorName: "Red", description: "Stop"),
+            TrafficLight(colorName: "Green", description: "Go"),
+            TrafficLight(colorName: "Yellow", description: "Attention")
+        ]
+
+        if let foundTrafficLight = trafficLight.first(where: {$0.colorName == colorName}) {
+            callBack(foundTrafficLight)
+        } else {
+            callBack(nil)
+        }
+    }
+}
